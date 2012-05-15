db4d5ac6f677ac0fc5c68b9c8794f800908448b8
Mayoto Yoshida <2getjp@gmail.com>
2012-05-15 14:14:36 +0900

    Add validate

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,6 @@ class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
 
   belongs_to :user
-  validates_presence_of :content
+  validates_presence_of :content, :user_id
   validates:content, :length => { :maximum => 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
   has_many :microposts
+
+  validates_presence_of :email, :name
+  validates_length_of :email, :maximum => 30
+  validates_length_of :name, :maximum => 30
 end
